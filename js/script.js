e0bffb766a40a32ec1cda4160bad15b7362fa57a
adinapop <adinampop@yahoo.com>
2021-06-07 16:41:34 +0200

    other ingredients added

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
-var calculateButton = document.getElementById("sum-button");
-var sumResult = document.getElementById("total-price");
+var calculateButton = document.getElementById("sum-button"); // creato la variabile con il button da cliccare
+
+var sumResult = document.getElementById("total-price"); // creata la variabile con il div per id dove stampare il prezzo finale
 
 calculateButton.addEventListener ('click', function() {
-    var startPrice = 0;
+    var sumPrice = 10; // prezzo base da sommare
     var checkIngredients = document.getElementsByClassName("ingredients");
+    // gli elementi da prendere per classe in modo tale da poter poi ciclare all'interno
 
     for (var i = 0; i < checkIngredients.length; i++) {
         if (checkIngredients[i].checked) {
-            startPrice += parseInt(checkIngredients[i].value)
+            sumPrice += parseInt(checkIngredients[i].value)
         }
     }
-
-    sumResult.innerHTML = "$ " + startPrice;
+    sumResult.innerHTML = "$ " + sumPrice; 
 })
+
+console.log(sumResult);
